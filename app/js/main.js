905883ea10a95ae0247d9f6cfa152b9ad67715a0
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-02-04 16:00:47 -0600

    js code for accessing json objects

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,35 @@
-;(function(){
+/* global $: false, _: false */
+
+(function () {
   'use strict';
 
-  console.log('NSS Template Loaded');
-}());
+  $(document).ready(function(){});
+
+  var FIREBASE_URL = 'https://savingsmultipliedssh.firebaseio.com/items.json';
+
+  $.get(FIREBASE_URL, function(data) {
+          console.log(data);
+    _.forEach(data, function(item){
+     var  endDate = item.endDate,
+          image = item.image,
+          price = item.price,
+          seller = item.seller,
+          title = item.title;
+
+
+  //CREATING A BLOCK OF ITEMS
+      var $figure = $('<figure><div><img src="' + image + '"></div><div>'
+                      + title + '</div><div>'
+                      +'$' + price + '</div><div>'
+                      + seller + '</div><div>'
+                      + endDate + '</div></figure>');
+       $('#itemDisplay').append($figure);
+     });
+   });
+
+
+
+
+
+
+})();
